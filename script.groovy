45e54f792d55266afe2c73573123442975f82474
Chad Dickerson <cdickersoncloudcoder@gmail.com>
2025-04-28 09:29:28 -0700

    Edit script.groovy

--- script.groovy
+++ script.groovy
@@ -6,9 +6,9 @@ def buildJar() {
 def buildImage() {
     echo "Building Docker image...."
     withCredentials([usernamePassword(credentialsId: "docker-hub-repo", passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t cdickersoncloudcoder/demo-app1.3:2.2 .'  // Building docker image
+        sh 'docker build -t cdickersoncloudcoder/demo-app1.3:2.3 .'  // Building docker image
         sh 'echo $PASS | docker login -u $USER --password-stdin'  // Login to Docker Hub
-        sh 'docker push cdickersoncloudcoder/demo-app1.3:2.2'  // Push the image to Docker Hub
+        sh 'docker push cdickersoncloudcoder/demo-app1.3:2.3'  // Push the image to Docker Hub
     }
 }
 
